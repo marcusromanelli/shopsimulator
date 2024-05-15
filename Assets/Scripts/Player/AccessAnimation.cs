b0bc08814fe70cbf210acdea13c13f16e72d1764
Marcus Romanelli <marcus.romanelli.m@gmail.com>
2024-05-15 11:57:17 -0300

    Adds WIP accessory loader

---
+++ Assets/Scripts/Player/AccessAnimation.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.U2D;
+using static PlayerMovement;
+
+[ExecuteInEditMode]
+public class AccessAnimation : MonoBehaviour
+{
+    public const int maxColumnCount = 8;
+    public const int maxHairCount = 18;
+
+    public const string hairSpritePrefix = "Accessories_";
+
+
+    [SerializeField, Range(0, maxHairCount)] private int accessoryIndex;
+    [SerializeField] private PlayerMovement playerMovement;
+    [SerializeField] private SpriteRenderer spriteRenderer;
+    [SerializeField] private SpriteAtlas accessorySpriteAtlas;
+    [SerializeField] private Vector2 offset;
+
+
+    private int lastSelectedAccIndex;
+
+    Vector2 lastDirection;
+    private void Awake()
+    {
+        playerMovement.onPlayerMoved += OnPlayerMoved;
+    }
+
+    void OnPlayerMoved(Vector2 direction)
+    {
+        lastDirection = direction;
+
+        LoadHair();
+    }
+
+    private void Update()
+    {
+        LoadHair();
+        transform.localPosition = offset;
+    }
+
+    void LoadHair()
+    {
+        var spriteName = GetSpriteName(lastDirection, accessoryIndex);
+
+        spriteRenderer.sprite = accessorySpriteAtlas.GetSprite(spriteName);
+
+    }
+    string GetSpriteName(Vector2 direction, int spriteIndex)
+    {
+        StringBuilder stringBuilder = new StringBuilder(hairSpritePrefix);
+
+
+        var realIndex = spriteIndex + Mathf.FloorToInt((float)spriteIndex / (float)maxColumnCount) * (maxColumnCount);
+
+        if (direction.y != 0) {
+            if (direction.y < 0)
+            {
+                lastSelectedAccIndex = realIndex;
+                stringBuilder.Append(lastSelectedAccIndex);
+
+                return stringBuilder.ToString();
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        if(direction.x != 0)
+        {
+            lastSelectedAccIndex = realIndex + maxColumnCount;
+            stringBuilder.Append(lastSelectedAccIndex);
+
+            return stringBuilder.ToString();
+        }
+
+        stringBuilder.Append(lastSelectedAccIndex);
+
+        return stringBuilder.ToString();
+    }
+}
